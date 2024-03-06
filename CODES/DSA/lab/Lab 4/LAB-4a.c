1278add9deacd309fcf32fff6cff75593e33ee26
tejppatil <tppatil397@gmail.com>
2024-03-06 14:01:18 +0530

    updated lab work 4

---
+++ CODES/DSA/lab/Lab 4/LAB-4a.c
@@ -0,0 +1,30 @@
+//Write a C program to implement Stack operations using Array.
+#include <stdio.h>
+int TOP=-1;
+int main() 
+{
+    int n,x;
+    printf("Enter the size of stack: ");
+    scanf("%d",&n);
+    int stack[n];
+    for(int i=0;i<=n;i++)
+    {
+        printf("Enter the Element");
+        scanf("%d",&x);
+        if(TOP==n-1)
+            {
+                printf("no space for-%d Stack is full,overflow condition",x);
+            }
+        else
+            {
+                TOP+=1;
+                stack[TOP]=x;
+            }
+    }
+    printf("\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",stack[i]);
+    }
+    return 0;
+}
