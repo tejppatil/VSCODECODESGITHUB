a0124367258c3bef89b35868f893f2898fc64e50
tejppatil <tppatil397@gmail.com>
2024-03-06 13:46:08 +0530

    Update lab work 2

---
+++ CODES/DSA/lab/Lab 2/LAB-2b.c
@@ -0,0 +1,36 @@
+//Write a C program to implement Binary Search.
+#include <stdio.h>
+int binarySearch(int array[], int low, int high, int x)
+{
+  if (high >= low) 
+  {
+    int mid = low + (high - low) / 2;
+    // If found at mid, then return it
+    if (array[mid] == x)
+      return mid;
+    // Search the left half
+    if (array[mid] > x)
+      return binarySearch(array,  low, mid - 1,x);
+    // Search the right half
+    return binarySearch(array, mid + 1, high, x);
+  }
+  return -1;
+}
+int main() 
+{
+    int size,ele;
+    printf("Enter the size of an array");
+    scanf("%d",&size);
+    printf("Enter the elemnt that you need to find in an array");
+    scanf("%d",&ele);
+    printf("Enter the elemnets in an array");
+    int arr[size];
+    for(int i=0;i<=size-1;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int result = binarySearch(arr, 0,size - 1, ele);
+    (result == -1) ? printf("Element is not present in array")
+                   : printf("Element is present at index %d",result);
+    return 0;
+}
