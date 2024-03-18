b37902483c7113f74684a1c1f3ac54c65c9a5122
tejppatil <tppatil397@gmail.com>
2024-03-18 11:20:34 +0530

    update lab work

---
+++ CODES/C++/LAB/18-03-24/.cpp
@@ -0,0 +1,20 @@
+//create class,object and adding program
+//using class and object two access specifier with any program
+//
+#include<iostream>
+using namespace std;
+class add
+{
+    Public:
+        int num1;
+        int num2; 
+}
+int main()
+{
+    add sum;
+    cout<<"Enter the 1st num: ";
+    cin>>sum.num1;
+    cout<<"Enter the 2nd num: ";
+    cin>>sum.num2;
+    cout<<"Sum = "<<sum.num1+sum.num2;
+}
