f341999ff5edb14204fe11e83f2a8df88e96a8ce
tejppatil <tppatil397@gmail.com>
2024-04-01 11:12:48 +0530

    work update

--- CODES/DSA/lab/Lab 9(Tryout)/duplicatechecker.c
+++ CODES/DSA/lab/Lab 9(Tryout)/duplicatechecker.c
@@ -2,11 +2,10 @@
 #include<stdio.h>
 int main()
 {
-    int a[100],n,i,j;
+    int n,i,j;
     printf("Enter the size of array: ");
     scanf("%d",&n);
-    
-    int b[a[i]];
+    int a[n];
     printf("Enter the elements of array:\n");
     for(i=0;i<n;i++)
     {
@@ -17,15 +16,16 @@ int main()
     {
         printf("%d ",a[i]);
     }
-    b[a[i]]++;
+    int count[n];
+    count[a[n]]++;
     for(i=0;i<n;i++)
     {
         for(j=i+1;j<n;j++)
         {
-            if(b[a[i]]>1)    
+            if(count[a[i]]>1)    
             {
                 printf("\n%d is Repeated in array",a[i]);
-                b[a[i]]=0;
+                count[a[i]]=0;
                 break;   
             }
         }
